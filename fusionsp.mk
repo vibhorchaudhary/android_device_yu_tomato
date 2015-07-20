e09d63e97ecab4d95a468ceb8b353ccca348722d
vibhu0009 <cvibhu0009@gmail.com>
2015-07-21 02:11:18 +0530

    initial commit

--- fusionsp.mk
+++ fusionsp.mk
@@ -14,10 +14,10 @@
 
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Fusion stuff.
+$(call inherit-product, vendor/fusion/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_tomato
+PRODUCT_NAME := fusion_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
 
